738a3937ef07416a72a8058f915769a17c5f6a84
Aldo Mata <Aldomata4516@gmail.com>
2025-04-25 17:50:52 -0600

    added prework to the repository

---
+++ codepathpt1Tests/codepathpt1Tests.swift
@@ -0,0 +1,17 @@
+//
+//  codepathpt1Tests.swift
+//  codepathpt1Tests
+//
+//  Created by Aldo Mata on 4/24/25.
+//
+
+import Testing
+@testable import codepathpt1
+
+struct codepathpt1Tests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
